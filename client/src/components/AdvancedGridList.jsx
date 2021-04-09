02a07039542603329e856d65a98026560647c474
bv-loop <72999113+bv-loop@users.noreply.github.com>
2021-04-09 19:26:52 +0100

    Change React components

---
+++ client/src/components/AdvancedGridList.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+
+/**
+ * The data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *     featured: true,
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+function AdvancedGridList(props) {
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+      backgroundImage: 'url("https://www.transparenttextures.com/patterns/asfalt-dark.png")',
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+      transform: 'translateZ(0)',
+    },
+    titleBar: {
+      background:
+        'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+        'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    icon: {
+      color: 'white',
+    },
+  }));
+
+  const classes = useStyles();
+
+  const defualtItems = ["“How to Achieve Results Using This One Weird Trick”","“You'll Never Believe This _________ “","“They Didn't Know _________ . Then This Happened …”"];
+
+  function handleDelete(event){
+    props.deleteOutcome(event.target.value)
+  }
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={200} spacing={1} className={classes.gridList}>
+        {props.outcomeList.map((outcomeItem, index) => (
+          <GridListTile key={index} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
+            <img src={outcomeItem.imgUrl} alt={outcomeItem.result} />
+            <GridListTileBar
+              title={outcomeItem.prediction}
+              titlePosition="top"
+              actionIcon={
+                <IconButton aria-label={`star ${outcomeItem.prediction}`} className={classes.icon}>
+                  { outcomeItem.prediction in defualtItems? <StarBorderIcon value={index}/>:<DeleteForeverIcon onClick={handleDelete} value={index}/>}
+                </IconButton>
+              }
+              actionPosition="left"
+              className={classes.titleBar}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
+
+export default AdvancedGridList;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import Form from './Form';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import Header from './Header';
 import Footer from './Footer';
+import AdvancedGridList from "./AdvancedGridList";
 
 function App(){
 
@@ -69,64 +64,19 @@ function App(){
             })
         }
 
-
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'space-around',
-          overflow: 'hidden',
-          backgroundImage: 'url("https://www.transparenttextures.com/patterns/asfalt-dark.png")',
-          marginTop: "70px"
-        },
-        gridList: {
-          flexWrap: 'nowrap',
-          transform: 'translateZ(0)',
-        
-        },
-        title: {
-          color: theme.palette.primary.light,
-        },
-        titleBar: {
-          background:
-            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-        }
-      }));
-
-      const classes = useStyles();
-
     return <div>
             <Header/>
             <h3 className="guidance">There's too much clickbait going around. Simply enter the name of the article and the site will predict whether the piece is clickbait or not. We'll generate an image with your result.</h3>
             <Form userInput={prediction} onFormChange={handleFormChange} onFormSubmit={handleFormSubmit}/>
             
-            <div className={classes.root}>
-            <GridList className={classes.gridList} cols={2.5} cellHeight={250}>
-            {outcome.map((outcomeItem, index) => (
-            <GridListTile key={index}>
-            <img src={outcomeItem.imgUrl} alt={outcomeItem.result} />
-            <GridListTileBar
-              title={outcomeItem.result}
-              subtitle={outcomeItem.prediction}
-              titlePosition={'bottom'}
-              classes={{
-                root: classes.titleBar,
-                title: classes.title,
-              }}
-              actionIcon={
-                <IconButton aria-label={`star ${outcomeItem.prediction}`} onClick={()=> deleteOutcome(index)}>
-                  <DeleteForeverIcon className={classes.title} />
-                </IconButton>
-              }
+            <AdvancedGridList
+              outcomeList={outcome}
+              onDelete={deleteOutcome}
             />
-            </GridListTile>
-        ))}
-      </GridList>
-    </div>
 
-    <Footer/>
+          <Footer/>
 
-    </div>
+          </div>
 }
 
 export default App;
